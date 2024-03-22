77f6c9a5313b6c1f207b39abb811f87951f522bd
Paul C <pacosgrove1@googlemail.com>
2024-03-22 17:18:28 +0000

    context menu for aspects

--- system/sheets/player.js
+++ system/sheets/player.js
@@ -14,13 +14,43 @@ export default class WildseaPlayerSheet extends ActorSheet {
 
   getData() {
     const context = super.getData()
-
     context.system = this.actor.system
-
     context.aspects = this.actor.itemTypes.aspect.sort((a, b) =>
       a.sort < b.sort ? -1 : 1,
     )
-    console.log(context.aspects)
+
     return context
   }
+
+  activateListeners(html) {
+    if (this.isEditable) {
+      if (this.actor.isOwner) {
+        if (this.actor.type === 'player') {
+          // Item context menu
+          new ContextMenu(html, '.itemContextMenu', this.itemContextMenu)
+        }
+      }
+    }
+
+    super.activateListeners(html)
+  }
+
+  itemContextMenu = [
+    {
+      name: game.i18n.localize('wildsea.edit'),
+      icon: '<i class="fas fa-edit"></i>',
+      callback: (element) => {
+        const itemId = element.closest('.item').data('item-id')
+        this.actor.items.get(itemId).sheet.render(true)
+      },
+    },
+    {
+      name: game.i18n.localize('wildsea.delete'),
+      icon: '<i class="fas fa-trash"></i>',
+      callback: (element) => {
+        const itemId = element.closest('.item').data('item-id')
+        this.actor.deleteEmbeddedDocuments('Item', [itemId])
+      },
+    },
+  ]
 }
